2e48cca75d77297f2e610c54bdd354e3b13153ea
Gaganjot Kaur Saini <kismat.saini@gmail.com>
2016-09-13 15:55:57 +1000

    Master version

--- src/library/entities/Book.java
+++ src/library/entities/Book.java
@@ -6,134 +6,70 @@ import library.interfaces.entities.ILoan;
 
 public class Book implements IBook {
 
-	
-	private String author;
-	private String title;
-	private String callNumber;
-	private int id;
-	
-	private ILoan loan;
-	private EBookState state;
-	
-	
-	public Book(String author, String title, String callNumber, int bookID) {
-		if ( !sane(author, title, callNumber, bookID)) {
-			throw new IllegalArgumentException("Member: constructor : bad parameters");
-		}
-		this.author = author;
-		this.title = title;
-		this.callNumber = callNumber;
-		this.id = bookID;
-		this.state = EBookState.AVAILABLE;
-		this.loan = null;
-	}
-
-
-	private boolean sane(String author, String title, String callNumber, int bookID) {
-		return  ( author != null     && !author.isEmpty()     &&
-				  title != null      && !title.isEmpty()      &&
-				  callNumber != null && !callNumber.isEmpty() &&
-				  bookID > 0 
-				);
-	}
-
-	
 	@Override
 	public void borrow(ILoan loan) {
-		if (loan == null) {
-			throw new IllegalArgumentException(String.format("Book: borrow : Bad parameter: loan cannot be null"));
-		}
-		if (!(state == EBookState.AVAILABLE)) {
-			throw new RuntimeException(String.format("Illegal operation in state : %s", state));
-		}
-		this.loan = loan;
-		state = EBookState.ON_LOAN;
+		// TODO Auto-generated method stub
 
 	}
 
-	
 	@Override
 	public ILoan getLoan() {
-		return loan;
+		// TODO Auto-generated method stub
+		return null;
 	}
 
-	
 	@Override
 	public void returnBook(boolean damaged) {
-		if (!(state == EBookState.ON_LOAN || state == EBookState.LOST)) {
-			throw new RuntimeException(String.format("Illegal operation in state : %s", state));
-		}
-		loan = null;
-		if (damaged) {
-			state = EBookState.DAMAGED;
-		}
-		else {
-			state = EBookState.AVAILABLE;
-		}
+		// TODO Auto-generated method stub
+
 	}
 
-	
 	@Override
 	public void lose() {
-		if (!(state == EBookState.ON_LOAN)) {
-			throw new RuntimeException(String.format("Illegal operation in state : %s", state));
-		}
-		state = EBookState.LOST;
+		// TODO Auto-generated method stub
+
 	}
 
-	
 	@Override
 	public void repair() {
-		if (!(state == EBookState.DAMAGED)) {
-			throw new RuntimeException(String.format("Illegal operation in state : %s", state));
-		}
-		state = EBookState.AVAILABLE;
+		// TODO Auto-generated method stub
+
 	}
 
-	
 	@Override
 	public void dispose() {
-		if (!(state == EBookState.AVAILABLE || state == EBookState.DAMAGED || state == EBookState.LOST)) {
-			throw new RuntimeException(String.format("Illegal operation in state : %s", state));
-		}
-		state = EBookState.DISPOSED;
+		// TODO Auto-generated method stub
+
 	}
 
-	
 	@Override
 	public EBookState getState() {
-		return state;
+		// TODO Auto-generated method stub
+		return null;
 	}
 
-	
 	@Override
 	public String getAuthor() {
-		return author;
+		// TODO Auto-generated method stub
+		return null;
 	}
 
-	
 	@Override
 	public String getTitle() {
-		return title;
+		// TODO Auto-generated method stub
+		return null;
 	}
 
-	
 	@Override
 	public String getCallNumber() {
-		return callNumber;
+		// TODO Auto-generated method stub
+		return null;
 	}
 
-	
 	@Override
 	public int getID() {
-		return id;
-	}
-
-	
-	@Override
-	public String toString() {
-		return String.format("Id: %d\nAuthor: %s\nTitle: %s\nCall Number %s",
-				id, author, title, callNumber);
+		// TODO Auto-generated method stub
+		return 0;
 	}
 
 }
