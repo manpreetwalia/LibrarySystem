22333b56a4a2edaddee59886788efc2afdcbed26
Gaganjot Kaur Saini <kismat.saini@gmail.com>
2016-09-18 18:12:23 +1000

    Add files via upload

---
+++ MemberHelperTest.java
@@ -0,0 +1,15 @@
+package library.daos;
+
+import junit.framework.TestCase;
+
+public class MemberHelperTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+}
