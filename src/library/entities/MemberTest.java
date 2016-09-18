5fad55be57f483f17e41f16e5fa87e05e5ec7587
Gaganjot Kaur Saini <kismat.saini@gmail.com>
2016-09-18 22:01:08 +1000

    MemberTest

---
+++ src/library/entities/MemberTest.java
@@ -0,0 +1,288 @@
+/**
+ * 
+ */
+package library.entities;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author swaraj
+ *
+ */
+public class MemberTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link library.entities.Member#Member(java.lang.String, java.lang.String, java.lang.String, java.lang.String, int)}.
+	 */
+	@Test
+	public final void testMember() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link library.entities.Member#hasOverDueLoans()}.
+	 */
+	@Test
+	public final void testHasOverDueLoans() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link library.entities.Member#hasReachedLoanLimit()}.
+	 */
+	@Test
+	public final void testHasReachedLoanLimit() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link library.entities.Member#hasFinesPayable()}.
+	 */
+	@Test
+	public final void testHasFinesPayable() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link library.entities.Member#hasReachedFineLimit()}.
+	 */
+	@Test
+	public final void testHasReachedFineLimit() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link library.entities.Member#getFineAmount()}.
+	 */
+	@Test
+	public final void testGetFineAmount() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link library.entities.Member#addFine(float)}.
+	 */
+	@Test
+	public final void testAddFine() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link library.entities.Member#payFine(float)}.
+	 */
+	@Test
+	public final void testPayFine() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link library.entities.Member#addLoan(library.interfaces.entities.ILoan)}.
+	 */
+	@Test
+	public final void testAddLoan() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link library.entities.Member#getLoans()}.
+	 */
+	@Test
+	public final void testGetLoans() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link library.entities.Member#removeLoan(library.interfaces.entities.ILoan)}.
+	 */
+	@Test
+	public final void testRemoveLoan() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link library.entities.Member#getState()}.
+	 */
+	@Test
+	public final void testGetState() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link library.entities.Member#getFirstName()}.
+	 */
+	@Test
+	public final void testGetFirstName() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link library.entities.Member#getLastName()}.
+	 */
+	@Test
+	public final void testGetLastName() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link library.entities.Member#getContactPhone()}.
+	 */
+	@Test
+	public final void testGetContactPhone() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link library.entities.Member#getEmailAddress()}.
+	 */
+	@Test
+	public final void testGetEmailAddress() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link library.entities.Member#getID()}.
+	 */
+	@Test
+	public final void testGetID() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link library.entities.Member#toString()}.
+	 */
+	@Test
+	public final void testToString() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#Object()}.
+	 */
+	@Test
+	public final void testObject() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#getClass()}.
+	 */
+	@Test
+	public final void testGetClass() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#hashCode()}.
+	 */
+	@Test
+	public final void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#equals(java.lang.Object)}.
+	 */
+	@Test
+	public final void testEquals() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#clone()}.
+	 */
+	@Test
+	public final void testClone() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#toString()}.
+	 */
+	@Test
+	public final void testToString1() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#notify()}.
+	 */
+	@Test
+	public final void testNotify() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#notifyAll()}.
+	 */
+	@Test
+	public final void testNotifyAll() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#wait(long)}.
+	 */
+	@Test
+	public final void testWaitLong() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#wait(long, int)}.
+	 */
+	@Test
+	public final void testWaitLongInt() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#wait()}.
+	 */
+	@Test
+	public final void testWait() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#finalize()}.
+	 */
+	@Test
+	public final void testFinalize() {
+		fail("Not yet implemented");
+	}
+
+}
