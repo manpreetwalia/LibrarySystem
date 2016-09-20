bc4e328b980b198e3b695af96acf2a488ccdc491
Manpreet Kaur Walia <manpreetwalia.2008@rediffmail.com>
2016-09-20 16:40:15 +1000

    loan Test

---
+++ src/library/entities/LoanTest.java
@@ -0,0 +1,26 @@
+package library.entities;
+
+import java.util.Date;
+
+
+
+public class LoanTest {
+	public static void main(String[] args){
+	
+		Book book1= new Book("Manpreet", " ABC","2343",444);
+		Member borrower1 = new Member("Preet", "Walia", "045231654", "manpreetwalia.2008@rediffmail.com", 2222);
+		
+		int year = 2016;
+		int month = 10;
+		int date = 02;
+		Date borrowDate = new Date(year,month,date);
+		Date dueDate = new Date(2016, 05, 06);
+		//Loan L= new Loan(book1, borrower1, borrowDate, dueDate);
+		
+	System.out.println("" + borrower1.getFirstName());
+	
+	
+	}
+	
+	
+}
--- src/library/entities/TestBook.java
+++ src/library/entities/TestBook.java
@@ -1,10 +1,8 @@
 package library.entities;
 
-
-
 public class TestBook {
-	public static void main(String[] args){
-		Book b1= new Book("MANpreet", " ABC","2343",444);
+	public static void main(String[] args) {
+		Book b1 = new Book("MANpreet", " ABC", "2343", 444);
 		System.out.println("THE author is:" + b1.getAuthor());
 		System.out.println("The title of book is:" + b1.getTitle());
 		System.out.println("The callNumber of book is:" + b1.getCallNumber());
@@ -13,10 +11,6 @@ public class TestBook {
 		System.out.println("The title of book is:" + b1.getID());
 		System.out.println("The state of book is:" + b1.getState());
 		System.out.println("The loan of book is:" + b1.getLoan());
-	
-	
+
 	}
-	
-	
-	
 }
