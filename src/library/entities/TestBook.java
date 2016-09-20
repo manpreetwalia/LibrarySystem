40eed35c1bf94bda1bd54b94cd7a6454e445e221
Manpreet Kaur Walia <manpreetwalia.2008@rediffmail.com>
2016-09-21 08:21:35 +1000

    Testing of Book Entity

--- src/library/entities/TestBook.java
+++ src/library/entities/TestBook.java
@@ -11,30 +11,30 @@ public class TestBook {
 		
 // Create object b1 of class book
 		
-		Book b1 = new Book("MANpreet", " ABC", "2343", 444);
+		Book b1 = new Book("Manpreet Walia", " Introductio to JAVA", "158643", 456);
 		
 // Test the function getAuthor()
-		System.out.println("The Name of author is:" + b1.getAuthor());
+		System.out.println("The Name of author is:    " + b1.getAuthor());
 		
 // Test the function getTitle()
 		
-		System.out.println("The title of book is:" + b1.getTitle());
+		System.out.println("The title of book is:    " + b1.getTitle());
 		
 // Test the function getCallNumber()
 		
-		System.out.println("The callNumber of book is:" + b1.getCallNumber());
+		System.out.println("The callNumber of book is:   " + b1.getCallNumber());
 		
 // Test the function getID()
-		System.out.println("The ID of book is:" + b1.getID());
+		System.out.println("The ID of book is:    " + b1.getID());
 		
 // Test the function toString()
-		System.out.println("Details of b1 are " + b1.toString());
+		System.out.println("Details of b1 are    " + b1.toString());
 		
 // Test the function getState()		
-		System.out.println("The state of book is:" + b1.getState());
+		System.out.println("The state of book is:     " + b1.getState());
 		
 // Test the function getLoan()
-		System.out.println("The loan of book is:" + b1.getLoan());
+		System.out.println("The loan of book is:   " + b1.getLoan());
 
 	}
 }
