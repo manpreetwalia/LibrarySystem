e35b01be2913a0b7e2c6458c183a7b22434c064c
Manpreet Kaur Walia <manpreetwalia.2008@rediffmail.com>
2016-09-21 18:14:38 +1000

    Testing

--- src/library/daos/BookMapDAOTest.java
+++ src/library/daos/BookMapDAOTest.java
@@ -1,4 +1,4 @@
-// Start of test class BookMapDAO
+ // Start of test class BookMapDAO
 
 package library.daos;
 
@@ -8,14 +8,18 @@ import java.util.Scanner;
 import library.entities.Book;
 
 // Start of TestBookMapDAO class
-public class BookMapDAOTest {
 
-	// Start of main function
-	public static void main(String[] args) {
-		String Author = null;//initialising string Author
-		String Title = null;// initialising the title
+public class BookMapDAOTest
+{
+
+// Start of main function
+	
+	public static void main(String[] args)
+	{
+		String Author = null;  // Initializing string Author
+		String Title = null;//  Initializing the title
 		
-		Scanner in = new Scanner(System.in);//Taking the input as Author
+		Scanner in = new Scanner(System.in);  //Taking the input as Author
 		 
 	      System.out.println("Enter Author name");//Inputs for Author name
 	      Author = in.nextLine();// Accesing input Author Name
--- src/library/daos/TestBookMapDAO.java
+++ src/library/daos/TestBookMapDAO.java
@@ -2,6 +2,8 @@
 
 package library.daos;
 
+import library.entities.Book;
+
 // Start of TestBookMapDAO class
 public class TestBookMapDAO {
 	
@@ -9,7 +11,12 @@ public class TestBookMapDAO {
 	public static void main(String[] args)
 	{
 		
-		
+		Book b1= new Book( "abc", "adsf","42165",23);
+ System.out.println("name is" + b1.getAuthor());
 
-}
+// System.out.println("The Name of author is:    " + b1.getAuthor());
+	
+	}
+		
+	
 }
