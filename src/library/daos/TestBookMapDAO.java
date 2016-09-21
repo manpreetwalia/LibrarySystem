ef16f14e441eb2a1a39d01a5603edb6a9fa5cdab
Manpreet Kaur Walia <manpreetwalia.2008@rediffmail.com>
2016-09-21 10:02:53 +1000

    Testing

--- src/library/daos/TestBookMapDAO.java
+++ src/library/daos/TestBookMapDAO.java
@@ -1,5 +1,15 @@
+// Start of test class BookMapDAO
+
 package library.daos;
 
+// Start of TestBookMapDAO class
 public class TestBookMapDAO {
+	
+// Start of main function
+	public static void main(String[] args)
+	{
+		
+// Create object b1 of class book
 
+}
 }
