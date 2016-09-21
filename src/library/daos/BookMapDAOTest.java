de5d7fc1f37fe6c362bcc57f3050047351577b81
aamerk2 <jawwadaamer@gmail.com>
2016-09-21 17:41:45 +1000

    Started the test case

--- src/library/daos/BookMapDAOTest.java
+++ src/library/daos/BookMapDAOTest.java
@@ -2,15 +2,28 @@
 
 package library.daos;
 
-// Start of TestBookMapDAO class
-public class BookMapDAOTest
-{
-	
-// Start of main function
-	public static void main(String[] args)
-	{
-		
-		
+import java.io.Console;
+import java.util.Scanner;
 
-}
+import library.entities.Book;
+
+// Start of TestBookMapDAO class
+public class BookMapDAOTest {
+
+	// Start of main function
+	public static void main(String[] args) {
+		String Author = null;
+		String Title = null;
+		
+		Scanner in = new Scanner(System.in);
+		 
+	      System.out.println("Enter Author name");
+	      Author = in.nextLine();
+	      System.out.println("You entered string "+s);
+	 
+	      System.out.println("Enter an integer");
+	      a = in.nextInt();
+	      System.out.println("You entered integer "+a);
+
+	}
 }
