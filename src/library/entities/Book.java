06e2b04e66c46b943173a7dd0a45b976cbdcb32b
manpreetwalia <manpreetwalia.2008@rediffmail.com>
2016-09-22 08:11:48 +1000

    Update Book.java

--- src/library/entities/Book.java
+++ src/library/entities/Book.java
@@ -3,73 +3,151 @@ package library.entities;
 import library.interfaces.entities.EBookState;
 import library.interfaces.entities.IBook;
 import library.interfaces.entities.ILoan;
+// Start of IBook class
 
 public class Book implements IBook {
 
-	@Override
-	public void borrow(ILoan loan) {
-		// TODO Auto-generated method stub
+	
+	private String author;  // Declare name of author as string
+	private String title;   // Declare title of book as string
+	private String callNumber;  // Declare call number of author as string
+	private int id;   //Declare the id of author as integer
+
+	// create instance of I loan
+	
+	private ILoan loan;
+	
+	// create instance of EBookSate
+	private EBookState state;
+	
+// Declare function Book which accepts four variables
+	
+	public Book(String author, String title, String callNumber, int bookID) 
+	{
+		if ( !sane(author, title, callNumber, bookID)) 
+		{
+			throw new IllegalArgumentException("Member: constructor : bad parameters");
+		}
 		
+		this.author = author;
+		this.title = title;
+		this.callNumber = callNumber;
+		this.id = bookID;
+		this.state = EBookState.AVAILABLE;
+		this.loan = null;
 	}
 
+
+	private boolean sane(String author, String title, String callNumber, int bookID) {
+		return  ( author != null     && !author.isEmpty()     &&
+				  title != null      && !title.isEmpty()      &&
+				  callNumber != null && !callNumber.isEmpty() &&
+				  bookID > 0 
+				);
+	}
+
+	
+	@Override
+	public void borrow(ILoan loan)
+	{
+		if (loan == null)
+		{
+			throw new IllegalArgumentException(String.format("Book: borrow : Bad parameter: loan cannot be null"));
+		}
+		
+		if (!(state == EBookState.AVAILABLE)) 
+		{
+			throw new RuntimeException(String.format("Illegal operation in state : %s", state));
+		}
+		
+		this.loan = loan;
+		state = EBookState.ON_LOAN;
+
+	}
+
+	// declare function getLoan()
 	@Override
 	public ILoan getLoan() {
-		// TODO Auto-generated method stub
-		return null;
+		return loan;
 	}
 
+	
 	@Override
 	public void returnBook(boolean damaged) {
-		// TODO Auto-generated method stub
-
+		if (!(state == EBookState.ON_LOAN || state == EBookState.LOST)) {
+			throw new RuntimeException(String.format("Illegal operation in state : %s", state));
+		}
+		loan = null;
+		if (damaged) {
+			state = EBookState.DAMAGED;
+		}
+		else {
+			state = EBookState.AVAILABLE;
+		}
 	}
-
+// declare function Lose() as void type
+	
 	@Override
 	public void lose() {
-		// TODO Auto-generated method stub
-
+		if (!(state == EBookState.ON_LOAN)) {
+			throw new RuntimeException(String.format("Illegal operation in state : %s", state));
+		}
+		state = EBookState.LOST;
 	}
 
+	// declare function repair as void type
 	@Override
 	public void repair() {
-		// TODO Auto-generated method stub
-
+		if (!(state == EBookState.DAMAGED)) {
+			throw new RuntimeException(String.format("Illegal operation in state : %s", state));
+		}
+		state = EBookState.AVAILABLE;
 	}
-
+	// declare function dispose as void type
+	
 	@Override
 	public void dispose() {
-		// TODO Auto-generated method stub
-
+		if (!(state == EBookState.AVAILABLE || state == EBookState.DAMAGED || state == EBookState.LOST)) {
+			throw new RuntimeException(String.format("Illegal operation in state : %s", state));
+		}
+		state = EBookState.DISPOSED;
 	}
 
+	// declare function getState()
 	@Override
 	public EBookState getState() {
-		// TODO Auto-generated method stub
-		return null;
+		return state;
 	}
 
+	// declare function getAuthor()
 	@Override
 	public String getAuthor() {
-		// TODO Auto-generated method stub
-		return null;
+		return author;
 	}
 
+	// declare function getTitle()
 	@Override
 	public String getTitle() {
-		// TODO Auto-generated method stub
-		return null;
+		return title;
 	}
 
+	// declare function getCallNumber()	
 	@Override
 	public String getCallNumber() {
-		// TODO Auto-generated method stub
-		return null;
+		return callNumber;
 	}
 
+	// declare function getID()	
 	@Override
 	public int getID() {
-		// TODO Auto-generated method stub
-		return 0;
+		return id;
+	}
+
+	// declare function toString()
+	@Override
+	public String toString() {
+		return String.format("Id: %d\nAuthor: %s\nTitle: %s\nCall Number %s",
+				id, author, title, callNumber);
 	}
 
 }
