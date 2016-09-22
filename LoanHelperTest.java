81d22a0a3f2219a95fc77abd286ad61db1e64c9e
Gaganjot Kaur Saini <kismat.saini@gmail.com>
2016-09-22 21:40:56 +1000

    GetBook and BorrowerDetails
    GetBook and BorrowerDetails Test

--- LoanHelperTest.java
+++ LoanHelperTest.java
@@ -1,119 +1,46 @@
-/**
- * 
- */
 package library.daos;
 
-import junit.framework.TestCase;
+//import java.util.Date;
+import library.entities.Book;
+import library.entities.Member;
+//import library.interfaces.entities.IBook;
+//import library.interfaces.entities.ILoan;
+//import library.interfaces.entities.IMember;
 
-/**
- * @author swaraj
- *
- */
-public class LoanHelperTest extends TestCase {
-
-	/* (non-Javadoc)
-	 * @see junit.framework.TestCase#setUp()
-	 */
-	protected void setUp() throws Exception {
-		super.setUp();
+public class LoanHelperTest {
+	public static void main(String[] args){
+		
+		Book book2 = new Book("Gurwinder Singh", "Database Management System", "+919876512345", 1);
+		Member borrower2 = new Member("Gagan", "Saini", "047894467", "gagansaini@gmail.com", 111);
+		
+		//int year = 2016;
+		//int month = 10;
+		//int date = 15;
+		//Date borrowDate = new Date(year,month,date);
+		boolean damaged = false;
+		System.out.println("Book details:");
+		System.out.println("");
+		System.out.println("The author name is " + book2.getAuthor());
+		System.out.println("The callNumber is: " + book2.getCallNumber());
+		System.out.println("ID: " + book2.getID());
+		System.out.println("Book title: " + book2.getTitle());		
+		System.out.println("Loan is " + book2.getLoan());
+		System.out.println("Book is " + book2.getState());
+		System.out.println("");
+		System.out.println("");
+		System.out.println("Borrower details:");
+		System.out.println("");
+		System.out.println("Contact Number: " + borrower2.getContactPhone());
+		System.out.println("Email ID: " + borrower2.getEmailAddress());
+		System.out.println("The fine amount is: " + borrower2.getFineAmount());
+		System.out.println("First Name: " + borrower2.getFirstName());
+		System.out.println("ID is: " + borrower2.getID());
+		System.out.println("Last Name: " + borrower2.getLastName());
+		System.out.println("Loan is: " + borrower2.getLoans());
+		System.out.println(borrower2.getState());
+		
+		System.out.println(damaged);
+		
+				//System.out.println("" + borrower2.getClass());		
 	}
-
-	/* (non-Javadoc)
-	 * @see junit.framework.TestCase#tearDown()
-	 */
-	protected void tearDown() throws Exception {
-		super.tearDown();
-	}
-
-	/**
-	 * Test method for {@link library.daos.LoanHelper#makeLoan(library.interfaces.entities.IBook, library.interfaces.entities.IMember, java.util.Date, java.util.Date)}.
-	 */
-	public void testMakeLoan() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for {@link java.lang.Object#Object()}.
-	 */
-	public void testObject() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for {@link java.lang.Object#getClass()}.
-	 */
-	public void testGetClass() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for {@link java.lang.Object#hashCode()}.
-	 */
-	public void testHashCode() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for {@link java.lang.Object#equals(java.lang.Object)}.
-	 */
-	public void testEquals() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for {@link java.lang.Object#clone()}.
-	 */
-	public void testClone() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for {@link java.lang.Object#toString()}.
-	 */
-	public void testToString() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for {@link java.lang.Object#notify()}.
-	 */
-	public void testNotify() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for {@link java.lang.Object#notifyAll()}.
-	 */
-	public void testNotifyAll() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for {@link java.lang.Object#wait(long)}.
-	 */
-	public void testWaitLong() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for {@link java.lang.Object#wait(long, int)}.
-	 */
-	public void testWaitLongInt() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for {@link java.lang.Object#wait()}.
-	 */
-	public void testWait() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for {@link java.lang.Object#finalize()}.
-	 */
-	public void testFinalize() {
-		fail("Not yet implemented");
-	}
-
 }
