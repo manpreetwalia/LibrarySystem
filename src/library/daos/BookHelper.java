4773d59af77576d70cdba17e3bf0562734008cd9
Manpreet Kaur Walia <manpreetwalia.2008@rediffmail.com>
2016-09-23 07:55:26 +1000

    testing

--- src/library/daos/BookHelper.java
+++ src/library/daos/BookHelper.java
@@ -9,6 +9,4 @@ public class BookHelper implements IBookHelper {
 	@Override
 	public IBook makeBook(String author, String title, String callNumber, int id) {
 		return new Book(author, title, callNumber, id);		
-	}
-
-}
+	}}
