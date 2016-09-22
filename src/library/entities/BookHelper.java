a3c164f04b9994b5d6b0689d58bf1f6978ef5c51
manpreetwalia <manpreetwalia.2008@rediffmail.com>
2016-09-23 08:57:37 +1000

    Update BookHelper.java

--- src/library/entities/BookHelper.java
+++ src/library/entities/BookHelper.java
@@ -1,9 +1,11 @@
 package library.daos;
-
+// import libray entities and interfaces
 import library.entities.Book;
 import library.interfaces.daos.IBookHelper;
 import library.interfaces.entities.IBook;
 
+
+// implement object of Bookhelper
 public class BookHelper implements IBookHelper {
 
 	@Override
