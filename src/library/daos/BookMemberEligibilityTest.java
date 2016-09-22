57092716c2239d4e33aa0a330d3933145d52d772
aamerk2 <jawwadaamer@gmail.com>
2016-09-23 00:34:43 +1000

    updated code

--- src/library/daos/BookMemberEligibilityTest.java
+++ src/library/daos/BookMemberEligibilityTest.java
@@ -8,10 +8,10 @@ public class BookMemberEligibilityTest {
 	{
 	    String member;
 		int member1= 100;
-		int member2 = 100;
-		int member3=100;
+		int member2 = 90;
+		int member3=80;
 		int member4= 100;
-		int member5=100;
+		int member5=10;
 		
 		
 		Scanner in = new Scanner(System.in);  //Taking the input as member
@@ -21,10 +21,11 @@ public class BookMemberEligibilityTest {
 		 
 		 if(member.equalsIgnoreCase("member1"))
 		 {
-			  member1=  member1 -10;//member1 loses book points for loan
-			  System.out.println();//printing the result value
+			  member1=  member1 - 10;//member1 loses book points for loan
+			  System.out.println("Remaining balance is : "+ member1);//printing the result value
 			  
 		 }
+		 
 	}
 
 }
