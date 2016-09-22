41d0e375fd1b96c1dfeb1ab73a44697b13bff255
Gaganjot Kaur Saini <kismat.saini@gmail.com>
2016-09-23 07:34:53 +1000

    LoanTestGborrowercontactTesing
    LoanTestGborrowercontact

--- LoanTestG.java
+++ LoanTestG.java
@@ -35,7 +35,7 @@ public class LoanTestG {
 		System.out.println("");
 		System.out.println("Borrower first name: " + borrower1.getFirstName());
 		System.out.println("Borrower last name: " + borrower1.getLastName());
-		//System.out.println("Borrower contact number: " + borrower1.getContactPhone());
+		System.out.println("Borrower contact number: " + borrower1.getContactPhone());
 		//System.out.println("Borrower EmailID: " + borrower1.getEmailAddress());
 		//System.out.println("Fine amount for this borrower is: " + borrower1.getFineAmount());
 		//System.out.println("Member(Borrower) ID is: " + borrower1.getID());
