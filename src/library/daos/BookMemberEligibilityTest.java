28fba01cd5eec55f6d9659c9689c4d9523205380
aamerk2 <jawwadaamer@gmail.com>
2016-09-23 00:42:12 +1000

    updated code

--- src/library/daos/BookMemberEligibilityTest.java
+++ src/library/daos/BookMemberEligibilityTest.java
@@ -32,7 +32,7 @@ public class BookMemberEligibilityTest {
 		 }
 		 else if(member.equalsIgnoreCase("member3"))
 		 {
-			 
+			 member3=  member3 - 10;
 		 }
 		 else if(member.equalsIgnoreCase("member4"))
 		 {
