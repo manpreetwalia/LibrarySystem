2a570636a6b866bc2572550f2cbf0935933852c1
mir mohd baqer <mirmohdbaqer@gmail.com>
2016-09-23 01:37:20 +1000

    Throws exception for helper tes

--- BookHelperTest.java
+++ BookHelperTest.java
@@ -7,7 +7,7 @@ public class BookHelperTest extends TestCase {
 	protected void setUp() throws Exception {
 		super.setUp();
 	}
-
+//Exception handing using throws clause
 	protected void tearDown() throws Exception {
 		super.tearDown();
 	}
