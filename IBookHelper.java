9eafd61a0ed509d5ed09727a681ff396777d172a
manpreetwalia <manpreetwalia.2008@rediffmail.com>
2016-09-23 09:43:59 +1000

    Update IBookHelper.java

--- IBookHelper.java
+++ IBookHelper.java
@@ -2,7 +2,8 @@ package library.interfaces.daos;
 
 import library.interfaces.entities.IBook; // import package
 
-public interface IBookHelper {
+public interface IBookHelper 
+{
 	
 	public IBook makeBook(String author, String title, String callNumber, int id);
 
