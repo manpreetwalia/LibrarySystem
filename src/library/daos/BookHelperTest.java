6e6a5fd028d72426be2854f26597cb7b7d274dd4
manpreetwalia <manpreetwalia.2008@rediffmail.com>
2016-09-23 08:40:09 +1000

    Update BookHelperTest.java

--- src/library/daos/BookHelperTest.java
+++ src/library/daos/BookHelperTest.java
@@ -37,7 +37,7 @@ public class BookHelperTest {
 		// Test the function getLoan()
 				System.out.println("The loan of book is:   " + book1.getLoan()); // display output
 
-		if(Author.equalsIgnoreCase("Y. Liang") && ID==123);
+		if(Author.equalsIgnoreCase("Y. Liang") && ID==123); // checks the condition
 		System.out.println(" The author of book is : Y. Liang");
 		
 		
