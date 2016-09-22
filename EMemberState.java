b9917eb77f545c718316abd002a6a3d8e19f15a9
manpreetwalia <manpreetwalia.2008@rediffmail.com>
2016-09-23 09:42:25 +1000

    Update EMemberState.java

--- EMemberState.java
+++ EMemberState.java
@@ -1,6 +1,7 @@
-package library.interfaces.entities; // package
+package library.interfaces.entities; 
 
-public enum EMemberState { 
+public enum EMemberState
+{ 
 	
 	BORROWING_ALLOWED, BORROWING_DISALLOWED 
 
