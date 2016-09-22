0692104a7c982bb4dc357308bb5e304ca8e76663
mir mohd baqer <mirmohdbaqer@gmail.com>
2016-09-22 22:21:57 +1000

    merging displays

--- src/library/interfaces/hardware/IDisplay.java
+++ src/library/interfaces/hardware/IDisplay.java
@@ -6,5 +6,5 @@ public interface IDisplay {
 	
 	public JPanel getDisplay(); // get function
 	public void setDisplay(JPanel panel, String identifier);
-	
+	public JPanel MegreDisplay();//Merging multiple displays
 }
