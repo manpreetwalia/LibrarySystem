2f63555f7559618bbcf6977c85a13fce6a617ad7
Manpreet Kaur Walia <manpreetwalia.2008@rediffmail.com>
2016-09-23 10:52:14 +1000

    Testing

--- src/library/entities/LoanTest.java
+++ src/library/entities/LoanTest.java
@@ -45,6 +45,7 @@ public class LoanTest {
 	
 	
 	// Test the getEmailAddress() function
+		
 	System.out.println("The E-mail address of borrower is:" + borrower1.getEmailAddress());
 
 	// Test the getID() function
@@ -56,21 +57,27 @@ public class LoanTest {
 	//System.out.println("The detail of loan is:" + borrower1.toString());
 	
 	// Test the getState()  function 
+	
 	System.out.println("The state of borrower is:" + borrower1.getState());
 	
 	// Test the getFineAmount function
+	
 	System.out.println("The fine of borrower is:" + borrower1.getFineAmount());
 	
 	// Test the hasReacehdLoanLimit() function
+	
 	System.out.println("Is borrower reached to the loan limit:" + borrower1.hasReachedLoanLimit());
 	
 	// Test the hasFinePAyable() function
+	
 	System.out.println("Has the borrower fine payable:" + borrower1.hasFinesPayable());
 	
 	// Test the hasReachedFineLimit() function
+	
 	System.out.println("IS borrower reached to the fine limit:" + borrower1.hasReachedFineLimit());
 	
 	// Test the getFineAmount() function
+	
 	System.out.println("The fine amount of borrower is:" + borrower1.getFineAmount());
 	
 	//
