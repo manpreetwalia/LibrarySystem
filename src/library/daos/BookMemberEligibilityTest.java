a6830f804099495cfd5a0181a749b2c24c85c004
aamerk2 <jawwadaamer@gmail.com>
2016-09-23 14:08:08 +1000

    code allignment

--- src/library/daos/BookMemberEligibilityTest.java
+++ src/library/daos/BookMemberEligibilityTest.java
@@ -3,51 +3,56 @@ package library.daos;
 import java.util.Scanner;
 
 public class BookMemberEligibilityTest {
-	
-	public static void main(String[] args)
-	{//Start
-	    String member;
-		int member1= 100;//static value for member
-		int member2 = 90;//static value for member
-		int member3=80;//static value for member
-		int member4= 100;//static value for member
-		int member5=10;//static value for member
-		
-		
-		Scanner in = new Scanner(System.in);  //Taking the input as member
-		 
-		 System.out.println("Enter the Author name");  //Inputs for member-name
-		 member = in.nextLine();// processing input
-		 
-		 if(member.equalsIgnoreCase("member1"))
-		 {
-			  member1=  member1 - 10;//member1 loses book points for loan
-			  System.out.println("Remaining balance is : "+ member1);//printing the result value
-			  
-		 }
-		 
-		 else if(member.equalsIgnoreCase("member2"))
-		 {
-			 member2=  member2 - 10;//member2 loses book points for loan
-			 System.out.println("Remaining balance is : "+ member2);//printing the result value
-		 }
-		 else if(member.equalsIgnoreCase("member3"))
-		 {
-			 member3=  member3 - 10;//member3 loses book points for loan
-			 System.out.println("Remaining balance is : "+ member3);//printing the result value
-		 }
-		 else if(member.equalsIgnoreCase("member4"))
-		 {
-			 member4=  member4 - 10;//member4 loses book points for loan
-			 System.out.println("Remaining balance is : "+ member4);//printing the result value
-		 }
-		 else if(member.equalsIgnoreCase("member5"))
-		 {
-			 member5=  member5 - 10;//member5 loses book points for loan
-			 System.out.println("Remaining balance is : "+ member5);//printing the result value
-		 }
-		 
+
+	public static void main(String[] args) {// Start
+		String member;
+		int member1 = 100;// static value for member
+		int member2 = 90;// static value for member
+		int member3 = 80;// static value for member
+		int member4 = 100;// static value for member
+		int member5 = 10;// static value for member
+
+		Scanner in = new Scanner(System.in); // Taking the input as member
+
+		System.out.println("Enter the Author name"); // Inputs for member-name
+		member = in.nextLine();// processing input
+
+		if (member.equalsIgnoreCase("member1")) {
+			member1 = member1 - 10;// member1 loses book points for loan
+			System.out.println("Remaining balance is : " + member1);// printing
+																	// the
+																	// result
+																	// value
+
+		}
+
+		else if (member.equalsIgnoreCase("member2")) {
+			member2 = member2 - 10;// member2 loses book points for loan
+			System.out.println("Remaining balance is : " + member2);// printing
+																	// the
+																	// result
+																	// value
+		} else if (member.equalsIgnoreCase("member3")) {
+			member3 = member3 - 10;// member3 loses book points for loan
+			System.out.println("Remaining balance is : " + member3);// printing
+																	// the
+																	// result
+																	// value
+		} else if (member.equalsIgnoreCase("member4")) {
+			member4 = member4 - 10;// member4 loses book points for loan
+			System.out.println("Remaining balance is : " + member4);// printing
+																	// the
+																	// result
+																	// value
+		} else if (member.equalsIgnoreCase("member5")) {
+			member5 = member5 - 10;// member5 loses book points for loan
+			System.out.println("Remaining balance is : " + member5);// printing
+																	// the
+																	// result
+																	// value
+		}
+
 	}
 
-}//end
-//testing completed successfully
+}// end
+	// testing completed successfully
