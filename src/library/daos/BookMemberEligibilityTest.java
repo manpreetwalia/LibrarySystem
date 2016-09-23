1b1a91613dd13c243ed073bf616db723be8ea5a1
aamerk2 <jawwadaamer@gmail.com>
2016-09-23 13:36:51 +1000

    updated code

--- src/library/daos/BookMemberEligibilityTest.java
+++ src/library/daos/BookMemberEligibilityTest.java
@@ -39,7 +39,7 @@ public class BookMemberEligibilityTest {
 		 else if(member.equalsIgnoreCase("member4"))
 		 {
 			 member4=  member4 - 10;//member4 loses book points for loan
-			 System.out.println("Remaining balance is : "+ member4);
+			 System.out.println("Remaining balance is : "+ member4);//printing the result value
 		 }
 		 else if(member.equalsIgnoreCase("member5"))
 		 {
