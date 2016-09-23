0cc6b57abac6738d6f0a3523df6f1e0a32b02448
aamerk2 <jawwadaamer@gmail.com>
2016-09-23 15:42:25 +1000

    Code allignment

--- src/library/interfaces/daos/IBookDAO.java
+++ src/library/interfaces/daos/IBookDAO.java
@@ -5,17 +5,17 @@ import java.util.List;
 import library.interfaces.entities.IBook;
 
 public interface IBookDAO {
-	
+
 	public IBook addBook(String author, String title, String callNo); // interface
-	
+
 	public IBook getBookByID(int id);
-	
+
 	public List<IBook> listBooks();
-	
+
 	public List<IBook> findBooksByAuthor(String author);
 
 	public List<IBook> findBooksByTitle(String title);
-	
+
 	public List<IBook> findBooksByAuthorTitle(String author, String title);
 
 }
