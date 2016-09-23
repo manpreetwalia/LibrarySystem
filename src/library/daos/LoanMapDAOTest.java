52d547f7266eb43f9d2ff6a2cbe32dd412b9ff16
aamerk2 <jawwadaamer@gmail.com>
2016-09-23 15:24:27 +1000

    Code Allignment

--- src/library/daos/LoanMapDAOTest.java
+++ src/library/daos/LoanMapDAOTest.java
@@ -1,5 +1,7 @@
 package library.daos;
 
+ 
+
 import library.entities.Book;
 
 import library.entities.Member;
@@ -8,28 +10,40 @@ import java.util.Date;
 
 import java.util.Calendar;
 
+ 
+
 public class LoanMapDAOTest {
 
-	public static void main(String[] args) {
+           
 
-		LoanHelper hlpr = new LoanHelper();
+                        public static void main(String[] args){
 
-		Date borrowDate = new Date();
+                                   
 
-		Date dueDate = new Date();
+                                    LoanHelper hlpr = new LoanHelper();                                  
 
-		Book book2 = new Book("Manpreet Walia", "Introduction to Java", "89656789", 5564);
+                                    Date borrowDate = new Date();
 
-		Member borrower2 = new Member("Manpreet", "Walia", "4566789", "manpreetwalia02@gmail.com", 856);
+                                    Date dueDate = new Date();                                                                       
 
-		LoanMapDAO lmd = new LoanMapDAO(hlpr);
+                                   
 
-		System.out.println(book2.getLoan());
+                                    Book book2 = new Book("Manpreet Walia", "Introduction to Java", "89656789", 5564);
 
-		System.out.println(lmd.getLoanByID(1));
+                                    Member borrower2 = new Member("Manpreet", "Walia", "4566789", "manpreetwalia02@gmail.com", 856);
 
-		System.out.println(hlpr.makeLoan(book2, borrower2, borrowDate, dueDate));
+                                   
 
-	}
+                                    LoanMapDAO lmd = new LoanMapDAO(hlpr);                             
+
+                                                                       
+
+                                    System.out.println(book2.getLoan());                                    
+
+                                    System.out.println(lmd.getLoanByID(1));
+
+                                    System.out.println(hlpr.makeLoan(book2, borrower2, borrowDate, dueDate));                  
+
+                        }
 
 }
