bcff9d83193d019958cb0393c7805751b5fd3310
aamerk2 <jawwadaamer@gmail.com>
2016-09-23 15:40:07 +1000

    Code allignment

--- src/library/entities/TestLoan.java
+++ src/library/entities/TestLoan.java
@@ -1,79 +1,94 @@
 package library.entities;
 
+ 
+
 import java.util.Date;
 
 import java.util.Calendar;
 
-public class TestLoan {
+ 
 
-	public static void main(String[] args)
+public class TestLoan
+{
 
-	{
+    
 
-		Date borrowDate = new Date();
+     public static void main(String[] args)
 
-		Date dueDate = new Date();
+     {
 
-		Book book1 = new Book("Manpret Walia", "Introduction to Database", "0672189", 5691);
+           Date borrowDate = new Date();                  
 
-		Member borrower1 = new Member("Preet", "Walia", "5646789", "preetwalia02@gmail.com", 444);
+           Date dueDate = new Date();
 
-		Loan l1 = new Loan(book1, borrower1, borrowDate, dueDate);
+           Book book1 = new Book("Manpret Walia", "Introduction to Database", "0672189",5691);
 
-		// Testing of loan class functions
-		System.out.println(l1.getBook());
+           Member borrower1 = new Member("Preet", "Walia", "5646789", "preetwalia02@gmail.com", 444);
 
-		System.out.println(l1.toString());
+           Loan l1 = new Loan(book1, borrower1, borrowDate, dueDate);
 
-		System.out.println(l1.getState());
+                    
+// Testing of loan class functions
+           System.out.println(l1.getBook());
 
-		System.out.println(l1.isOverDue());
+           System.out.println(l1.toString());        
 
-		System.out.println("" + l1.getID());
+           System.out.println(l1.getState());        
 
-		// Testing of memebre class functions
+           System.out.println(l1.isOverDue());       
 
-		System.out.println("" + borrower1.getFirstName());
+           System.out.println("" + l1.getID());
 
-		System.out.println("" + borrower1.getLastName());
+               
+// Testing of memebre class functions
+                     
 
-		System.out.println("" + borrower1.getContactPhone());
+           System.out.println("" + borrower1.getFirstName());
 
-		System.out.println("" + borrower1.getEmailAddress());
+           System.out.println("" + borrower1.getLastName());
 
-		System.out.println("" + borrower1.getFineAmount());
+           System.out.println("" + borrower1.getContactPhone());
 
-		System.out.println("" + borrower1.getID());
+           System.out.println("" + borrower1.getEmailAddress());
 
-		System.out.println("" + borrower1.getClass());
+           System.out.println("" + borrower1.getFineAmount());
 
-		System.out.println("" + borrower1.getLoans());
+           System.out.println("" + borrower1.getID());
 
-		System.out.println("" + borrower1.getState());
+           System.out.println("" + borrower1.getClass());
 
-		// Testing of Book class
-		System.out.println("" + book1.getAuthor());
+           System.out.println("" + borrower1.getLoans());
 
-		System.out.println("" + book1.getCallNumber());
+           System.out.println("" + borrower1.getState());
 
-		System.out.println("" + book1.getID());
+          
+// Testing of Book class
+           System.out.println("" + book1.getAuthor());
 
-		System.out.println("" + book1.getTitle());
+           System.out.println("" + book1.getCallNumber());
 
-		System.out.println("" + book1.getClass());
+           System.out.println("" + book1.getID());
 
-		System.out.println("" + book1.getState());
+           System.out.println("" + book1.getTitle());
 
-		System.out.println("" + borrowDate.getDate());
+           System.out.println("" + book1.getClass());
 
-	}
+           System.out.println("" + book1.getState());
 
-	private static Date addDays(Date borrowDate, int i) {
-
-		// TODO Auto-generated method stub
-
-		return null;
-
-	}
+           System.out.println("" + borrowDate.getDate()); 
 
 }
+
+ 
+
+     private static Date addDays(Date borrowDate, int i) 
+     {
+
+           // TODO Auto-generated method stub             
+
+           return null;
+
+     }
+
+}
+
