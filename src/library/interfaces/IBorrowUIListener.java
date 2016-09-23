f9b0ac5dc17c1465fb682219bc4951356bb37901
aamerk2 <jawwadaamer@gmail.com>
2016-09-23 15:41:54 +1000

    Code allignment

--- src/library/interfaces/IBorrowUIListener.java
+++ src/library/interfaces/IBorrowUIListener.java
@@ -1,14 +1,13 @@
 package library.interfaces;
 
-public interface IBorrowUIListener 
-{
+public interface IBorrowUIListener {
 	// functions declare as void type
-	public void cancelled(); 
-	
+	public void cancelled();
+
 	public void scansCompleted();
-	
+
 	public void loansConfirmed();
-	
+
 	public void loansRejected();
 
 }
