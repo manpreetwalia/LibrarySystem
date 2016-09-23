c272622c992c513990ccd03d21bcb2f0b3b4b83f
Manpreet Kaur Walia <manpreetwalia.2008@rediffmail.com>
2016-09-23 10:46:15 +1000

    Testing

--- src/library/daos/BookHelperTest.java
+++ src/library/daos/BookHelperTest.java
@@ -13,8 +13,11 @@ public class BookHelperTest {
 	{
 		
 // Create object book1 of class IBook, makebook
+		
 		Book book1 = new Book("Japgun", "Database", "98765432", 6666);
+		
 		// Test the function getAuthor()
+		
 				System.out.println("The Name of author is:    " + book1.getAuthor());
 				
 		// Test the function getTitle()
@@ -26,15 +29,19 @@ public class BookHelperTest {
 				System.out.println("The callNumber of book is:   " + book1.getCallNumber());
 				
 		// Test the function getID()
+				
 				System.out.println("The ID of book is:    " + book1.getID());
 				
 		// Test the function toString()
+				
 				System.out.println("Details of b1 are    " + book1.toString());
 				
 		// Test the function getState()		
+				
 				System.out.println("The state of book is:     " + book1.getState());
 				
 		// Test the function getLoan()
+				
 				System.out.println("The loan of book is:   " + book1.getLoan());
 
 			}
