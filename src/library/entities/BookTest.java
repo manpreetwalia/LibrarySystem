b9a745af5fa343ffcc2c5ff7f681ad89a8d92932
aamerk2 <jawwadaamer@gmail.com>
2016-09-23 15:28:32 +1000

    Code Allignment

--- src/library/entities/BookTest.java
+++ src/library/entities/BookTest.java
@@ -14,7 +14,6 @@ public class BookTest
 		Book b1 = new Book("Manpreet Walia", " Introductio to JAVA", "158643", 456);
 		
 // Test the function getAuthor()
-		
 		System.out.println("The Name of author is:    " + b1.getAuthor());
 		
 // Test the function getTitle()
@@ -26,22 +25,17 @@ public class BookTest
 		System.out.println("The callNumber of book is:   " + b1.getCallNumber());
 		
 // Test the function getID()
-		
 		System.out.println("The ID of book is:    " + b1.getID());
 		
 // Test the function toString()
-		
 		System.out.println("Details of b1 are    " + b1.toString());
 		
 // Test the function getState()	
-		
 		System.out.println("The state of book is:     " + b1.getState());
 		
 // Test the function getLoan()
-		
 		System.out.println("The loan of book is:   " + b1.getLoan());
-
+// Test the function 
 
 	}
-	
 }
