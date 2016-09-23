f74092c2aa544452d9061135013ae26bdbabcd2d
aamerk2 <jawwadaamer@gmail.com>
2016-09-23 15:44:10 +1000

    Code allignment

--- src/library/interfaces/daos/IMemberDAO.java
+++ src/library/interfaces/daos/IMemberDAO.java
@@ -6,16 +6,16 @@ import library.interfaces.entities.IMember;
 
 public interface IMemberDAO {
 
-		public IMember addMember(String firstName, String lastName, String ContactPhone, String emailAddress);
-		
-		public IMember getMemberByID(int id);
-		
-		public List<IMember> listMembers();
-		
-		public List<IMember> findMembersByLastName(String lastName);
+	public IMember addMember(String firstName, String lastName, String ContactPhone, String emailAddress);
 
-		public List<IMember> findMembersByEmailAddress(String emailAddress);
-		
-		public List<IMember> findMembersByNames(String firstName, String lastName);
+	public IMember getMemberByID(int id);
+
+	public List<IMember> listMembers();
+
+	public List<IMember> findMembersByLastName(String lastName);
+
+	public List<IMember> findMembersByEmailAddress(String emailAddress);
+
+	public List<IMember> findMembersByNames(String firstName, String lastName);
 
 }
