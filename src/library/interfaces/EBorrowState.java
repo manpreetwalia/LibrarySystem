998a8482a676cd5af90c3915eaa66ccd8652cb9a
Manpreet Kaur Walia <manpreetwalia.2008@rediffmail.com>
2016-09-23 11:03:42 +1000

    settings

--- src/library/interfaces/EBorrowState.java
+++ src/library/interfaces/EBorrowState.java
@@ -1,6 +1,7 @@
-package library.interfaces;  // interface
+package library.interfaces;  // interface of package library
 
-public enum EBorrowState {
+public enum EBorrowState
+{
 	CREATED, INITIALIZED, SCANNING_BOOKS, CONFIRMING_LOANS, COMPLETED, BORROWING_RESTRICTED, CANCELLED 
 
 }
