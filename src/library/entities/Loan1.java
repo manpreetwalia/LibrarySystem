0166398c5992c8ca8ca4d4e49b1040eddfc74fbf
aamerk2 <jawwadaamer@gmail.com>
2016-09-23 15:30:43 +1000

    Code Allignment

--- src/library/entities/Loan1.java
+++ src/library/entities/Loan1.java
@@ -8,8 +8,7 @@ import library.interfaces.entities.ILoan;
 import library.interfaces.entities.IMember;
 import library.interfaces.entities.ELoanState;
 
-public class Loan1 implements ILoan 
-{
+public class Loan1 implements ILoan {
 
 	private int id;
 	private final IMember borrower;
@@ -17,43 +16,31 @@ public class Loan1 implements ILoan
 	private Date borrowDate;
 	private Date dueDate;
 	private ELoanState state;
-	
-	public Loan1(IBook book, IMember borrower, Date borrowDate, Date returnDate) 
-	{
-		if (!sane(book, borrower, borrowDate, returnDate)) 
-		{
+
+	public Loan1(IBook book, IMember borrower, Date borrowDate, Date returnDate) {
+		if (!sane(book, borrower, borrowDate, returnDate)) {
 			throw new IllegalArgumentException("Loan: constructor : bad parameters");
 		}
 		this.book = book;
 		this.borrower = borrower;
 		this.borrowDate = borrowDate;
-		this.dueDate = returnDate;	
+		this.dueDate = returnDate;
 		this.state = ELoanState.PENDING;
 	}
-	
-	private boolean sane(IBook book, IMember borrower, Date borrowDate, Date returnDate) 
-	{
-		return  ( book != null && 
-				  borrower != null && 
-				  borrowDate != null && 
-				  returnDate != null && 
-				  borrowDate.compareTo(returnDate) <= 0);
+
+	private boolean sane(IBook book, IMember borrower, Date borrowDate, Date returnDate) {
+		return (book != null && borrower != null && borrowDate != null && returnDate != null
+				&& borrowDate.compareTo(returnDate) <= 0);
 	}
 
 	@Override
-	public void commit(int loanId)
-	{
-		if (!(state == ELoanState.PENDING))
-		{
-			throw new RuntimeException(
-					String.format("Loan : commit : incorrect state transition  : %s -> %s\n", 
-							state, ELoanState.CURRENT));
+	public void commit(int loanId) {
+		if (!(state == ELoanState.PENDING)) {
+			throw new RuntimeException(String.format("Loan : commit : incorrect state transition  : %s -> %s\n", state,
+					ELoanState.CURRENT));
 		}
-		if (loanId <= 0)
-		{
-			throw new RuntimeException(
-					String.format("Loan : commit : id must be a positive integer  : %d\n", 
-							loanId));
+		if (loanId <= 0) {
+			throw new RuntimeException(String.format("Loan : commit : id must be a positive integer  : %d\n", loanId));
 		}
 		this.id = loanId;
 		state = ELoanState.CURRENT;
@@ -62,70 +49,57 @@ public class Loan1 implements ILoan
 	}
 
 	@Override
-	public void complete() 
-	{
-		if (!(state == ELoanState.CURRENT || state == ELoanState.OVERDUE)) 
-		{
-			throw new RuntimeException(
-					String.format("Loan : complete : incorrect state transition  : %s -> %s\n",
-							state, ELoanState.COMPLETE));
+	public void complete() {
+		if (!(state == ELoanState.CURRENT || state == ELoanState.OVERDUE)) {
+			throw new RuntimeException(String.format("Loan : complete : incorrect state transition  : %s -> %s\n",
+					state, ELoanState.COMPLETE));
 		}
-		state = ELoanState.COMPLETE;		
+		state = ELoanState.COMPLETE;
 	}
 
 	@Override
-	public boolean isOverDue()
-	{
+	public boolean isOverDue() {
 		return (state == ELoanState.OVERDUE);
 	}
 
 	@Override
-	public boolean checkOverDue(Date currentDate) 
-	
+	public boolean checkOverDue(Date currentDate)
+
 	{
-		if (!(state == ELoanState.CURRENT || state == ELoanState.OVERDUE )) {
-			throw new RuntimeException(
-					String.format("Loan : checkOverDue : incorrect state transition  : %s -> %s\n",
-							state, ELoanState.OVERDUE));
+		if (!(state == ELoanState.CURRENT || state == ELoanState.OVERDUE)) {
+			throw new RuntimeException(String.format("Loan : checkOverDue : incorrect state transition  : %s -> %s\n",
+					state, ELoanState.OVERDUE));
 		}
-		if (currentDate.compareTo(dueDate) > 0) 
-		{
+		if (currentDate.compareTo(dueDate) > 0) {
 			state = ELoanState.OVERDUE;
 		}
 		return isOverDue();
 	}
 
 	@Override
-	public IMember getBorrower() 
-	{
+	public IMember getBorrower() {
 		return borrower;
 	}
 
 	@Override
-	public IBook getBook() 
-	{
+	public IBook getBook() {
 		return book;
 	}
 
 	@Override
-	public int getID()
-	{
+	public int getID() {
 		return id;
 	}
-	
-	public ELoanState getState() 
-	{
+
+	public ELoanState getState() {
 		return state;
 	}
 
 	@Override
-	public String toString() 
-	{
-		return (String.format("Loan ID:  %d\nAuthor:   %s\nTitle:    %s\nBorrower: %s %s\nBorrowed: %s\nDue Date: %s", 
+	public String toString() {
+		return (String.format("Loan ID:  %d\nAuthor:   %s\nTitle:    %s\nBorrower: %s %s\nBorrowed: %s\nDue Date: %s",
 				id, book.getAuthor(), book.getTitle(), borrower.getFirstName(), borrower.getLastName(),
-				DateFormat.getDateInstance().format(borrowDate),
-				DateFormat.getDateInstance().format(dueDate)));
+				DateFormat.getDateInstance().format(borrowDate), DateFormat.getDateInstance().format(dueDate)));
 	}
-
 
 }
