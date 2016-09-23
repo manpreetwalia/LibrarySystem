61c95e864cfc59f0e2403c0ea8359e9212476799
aamerk2 <jawwadaamer@gmail.com>
2016-09-23 15:26:41 +1000

    Code Allignment

--- src/library/entities/Book.java
+++ src/library/entities/Book.java
@@ -4,23 +4,18 @@ import library.interfaces.entities.EBookState;
 import library.interfaces.entities.IBook;
 import library.interfaces.entities.ILoan;
 
-public class Book implements IBook
-{
+public class Book implements IBook {
 
-	
 	private String author;
 	private String title;
 	private String callNumber;
 	private int id;
-	
+
 	private ILoan loan;
 	private EBookState state;
-	
-	
-	public Book(String author, String title, String callNumber, int bookID)
-	{
-		if ( !sane(author, title, callNumber, bookID)) 
-		{
+
+	public Book(String author, String title, String callNumber, int bookID) {
+		if (!sane(author, title, callNumber, bookID)) {
 			throw new IllegalArgumentException("Member: constructor : bad parameters");
 		}
 		this.author = author;
@@ -31,25 +26,17 @@ public class Book implements IBook
 		this.loan = null;
 	}
 
-
 	private boolean sane(String author, String title, String callNumber, int bookID) {
-		return  ( author != null     && !author.isEmpty()     &&
-				  title != null      && !title.isEmpty()      &&
-				  callNumber != null && !callNumber.isEmpty() &&
-				  bookID > 0 
-				);
+		return (author != null && !author.isEmpty() && title != null && !title.isEmpty() && callNumber != null
+				&& !callNumber.isEmpty() && bookID > 0);
 	}
 
-	
 	@Override
-	public void borrow(ILoan loan)
-	{
-		if (loan == null)
-		{
+	public void borrow(ILoan loan) {
+		if (loan == null) {
 			throw new IllegalArgumentException(String.format("Book: borrow : Bad parameter: loan cannot be null"));
 		}
-		if (!(state == EBookState.AVAILABLE))
-		{
+		if (!(state == EBookState.AVAILABLE)) {
 			throw new RuntimeException(String.format("Illegal operation in state : %s", state));
 		}
 		this.loan = loan;
@@ -57,106 +44,77 @@ public class Book implements IBook
 
 	}
 
-	
 	@Override
-	public ILoan getLoan() 
-	{
+	public ILoan getLoan() {
 		return loan;
 	}
 
-	
 	@Override
-	public void returnBook(boolean damaged)
-	{
-		if (!(state == EBookState.ON_LOAN || state == EBookState.LOST))
-		{
+	public void returnBook(boolean damaged) {
+		if (!(state == EBookState.ON_LOAN || state == EBookState.LOST)) {
 			throw new RuntimeException(String.format("Illegal operation in state : %s", state));
 		}
 		loan = null;
-		if (damaged)
-		{
+		if (damaged) {
 			state = EBookState.DAMAGED;
-		}
-		else
-		{
+		} else {
 			state = EBookState.AVAILABLE;
 		}
 	}
 
-	
 	@Override
-	public void lose()
-	{
-		if (!(state == EBookState.ON_LOAN)) 
-		{
+	public void lose() {
+		if (!(state == EBookState.ON_LOAN)) {
 			throw new RuntimeException(String.format("Illegal operation in state : %s", state));
 		}
-		
+
 		state = EBookState.LOST;
 	}
 
-	
 	@Override
-	public void repair() 
-	{
-		if (!(state == EBookState.DAMAGED)) 
-		{
+	public void repair() {
+		if (!(state == EBookState.DAMAGED)) {
 			throw new RuntimeException(String.format("Illegal operation in state : %s", state));
 		}
 		state = EBookState.AVAILABLE;
 	}
 
-	
 	@Override
-	public void dispose()
-	{
+	public void dispose() {
 		if (!(state == EBookState.AVAILABLE || state == EBookState.DAMAGED || state == EBookState.LOST)) {
 			throw new RuntimeException(String.format("Illegal operation in state : %s", state));
 		}
 		state = EBookState.DISPOSED;
 	}
 
-	
 	@Override
-	public EBookState getState() 
-	{
+	public EBookState getState() {
 		return state;
 	}
 
-	
 	@Override
-	public String getAuthor() 
-	{
+	public String getAuthor() {
 		return author;
 	}
 
-	
 	@Override
-	public String getTitle()
-	{
+	public String getTitle() {
 		return title;
 	}
 
-	
 	@Override
-	public String getCallNumber() 
-	{
+	public String getCallNumber() {
 		return callNumber;
 	}
 
-	
 	@Override
-	public int getID() 
-	{
+	public int getID() {
 		return id;
 	}
 
-	
 	@Override
-	public String toString() 
-	{
-		return String.format("Id: %d\nAuthor: %s\nTitle: %s\nCall Number %s",
-				id, author, title, callNumber);
+	public String toString() {
+		return String.format("Id: %d\nAuthor: %s\nTitle: %s\nCall Number %s", id, author, title, callNumber);
 	}
 
 }
