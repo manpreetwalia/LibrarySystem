c9360639f2b0d2429859671ac13bdd80f19ec65a
Manpreet Kaur Walia <manpreetwalia.2008@rediffmail.com>
2016-09-23 10:44:21 +1000

    Testing

--- src/library/daos/TestBookHelper.java
+++ src/library/daos/TestBookHelper.java
@@ -36,6 +36,7 @@ public class TestBookHelper {
 				
 		// Test the function toString()
 				System.out.println("Details of b1 are    " + b1.toString());
+				System.out.println("Details of b1 are    " + Bk.toString());
 				
 		// Test the function getState()		
 				System.out.println("The state of book is:     " + b1.getState());
