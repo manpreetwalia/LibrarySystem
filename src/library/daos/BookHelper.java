5a4c8ffa5b90426b6f5be1a3ebd6e7b023d7b496
Manpreet Kaur Walia <manpreetwalia.2008@rediffmail.com>
2016-09-23 10:46:42 +1000

    Testing

--- src/library/daos/BookHelper.java
+++ src/library/daos/BookHelper.java
@@ -4,10 +4,12 @@ import library.entities.Book;
 import library.interfaces.daos.IBookHelper;
 import library.interfaces.entities.IBook;
 
-public class BookHelper implements IBookHelper {
+public class BookHelper implements IBookHelper
+{
 
 	@Override
 	public IBook makeBook(String author, String title, String callNumber, int id)
+	
 	{
 		
 		return new Book(author, title, callNumber, id);		
