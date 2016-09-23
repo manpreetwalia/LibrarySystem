2a24ec6f967f86461691a4e37269f3c7269a47fc
aamerk2 <jawwadaamer@gmail.com>
2016-09-23 15:41:27 +1000

    Code allignment

--- src/library/interfaces/EBorrowState.java
+++ src/library/interfaces/EBorrowState.java
@@ -1,7 +1,6 @@
-package library.interfaces;  // interface of package library
+package library.interfaces; // interface of package library
 
-public enum EBorrowState
-{
-	CREATED, INITIALIZED, SCANNING_BOOKS, CONFIRMING_LOANS, COMPLETED, BORROWING_RESTRICTED, CANCELLED 
+public enum EBorrowState {
+	CREATED, INITIALIZED, SCANNING_BOOKS, CONFIRMING_LOANS, COMPLETED, BORROWING_RESTRICTED, CANCELLED
 
 }
